463de74752bf25dfaba688de8125ccf3ea6584c2
Utsav Rawat <utsav.rawat@sjsu.edu>
2024-02-19 12:30:23 +0530

    add navbar layout for sign-out

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import Navbar from "@/components/navbar";
+
 const DashboardLayout = ({
     children
 }: {
@@ -5,12 +7,16 @@ const DashboardLayout = ({
 }) => {
     return (
         <div className="h-full relative">
-            <div className="hidden h-full md:flex
+            <div className="hidden h-full md:flex md:w-72 
             md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
                 <div>
                     Hello Sidebar
                 </div>
             </div>
+            <main className="md:pl-72">
+                <Navbar />
+                {children}
+            </main>
         </div>
     );
 }
